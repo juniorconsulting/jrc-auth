a46d8ae0c032407e2a7445e10a2cbd83317acd20
Øyvind Robertsen <oyvind@arcticstyle.net>
2016-03-01 00:40:38 +0100

    add staticroot to urls

--- jrc_auth/urls.py
+++ jrc_auth/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from jrc_auth.api import router, login, check_token, logout
 
@@ -11,4 +13,5 @@ urlpatterns = [
     url(r'^logout/', logout),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth', include('rest_framework.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+

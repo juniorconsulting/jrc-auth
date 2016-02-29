2b66d27eccf861fba41e48379746ca7735655bb3
Øyvind Robertsen <oyvind@arcticstyle.net>
2016-02-29 13:39:50 +0100

    add basic auth flow support

--- jrc_auth/api.py
+++ jrc_auth/api.py
@@ -1,5 +1,14 @@
+from uuid import uuid4, UUID
+
 from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets, permissions
+from django.contrib.auth import authenticate
+from rest_framework import routers, serializers, viewsets, permissions, status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+
+# Two dictionaries for easy reverse lookup.
+logged_in_token = {}
+logged_in_userid = {}
 
 
 # Serializers define the API representation.
@@ -24,6 +33,51 @@ class UserViewSet(viewsets.ModelViewSet):
             return User.objects.all()
         else:
             return User.objects.filter(id=self.request.user.id)
+
+
+@api_view(['POST'])
+@permission_classes((permissions.AllowAny,))
+def login(request, format=None):
+    username = request.data['username']
+    password = request.data['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.id not in logged_in_userid:
+            token = uuid4()
+            logged_in_token[token] = user.id
+            logged_in_userid[user.id] = token
+        else:
+            token = logged_in_userid[user.id]
+        return Response({'token': token, 'userid': user.id})
+    return Response("Invalid username/password.", status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+@permission_classes((permissions.AllowAny,))
+def check_token(request, format=None):
+    token_str = request.data.get('token', None)
+    if token_str is not None:
+        token = UUID(token_str)
+    else:
+        return Response("Token required.", status=status.HTTP_400_BAD_REQUEST)
+    if token in logged_in_token:
+        return Response({"userid": logged_in_token[token]})
+    return Response("Invalid token.", status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+@permission_classes((permissions.AllowAny,))
+def logout(request, format=None):
+    token_str = request.data.get('token', None)
+    if token_str is not None:
+        token = UUID(token_str)
+    else:
+        return Response("Token required.", status=status.HTTP_400_BAD_REQUEST)
+    if token in logged_in_token:
+        userid = logged_in_token[token]
+        del logged_in_token[token]
+        del logged_in_userid[userid]
+    return Response("User logged out.")
 
 
 
--- jrc_auth/urls.py
+++ jrc_auth/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from jrc_auth.api import router
+from jrc_auth.api import router, login, check_token, logout
 
 
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^login/', login),
+    url(r'^check-token', check_token),
+    url(r'^logout/', logout),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth', include('rest_framework.urls'))
 ]

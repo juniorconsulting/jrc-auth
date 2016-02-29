5e22b5ca8be09fe981d5977e14e9ab2ddf6c19c5
Øyvind Robertsen <oyvind@arcticstyle.net>
2016-02-29 20:15:48 +0100

    use redis to store tokens/userids

--- jrc_auth/api.py
+++ jrc_auth/api.py
@@ -5,11 +5,9 @@ from django.contrib.auth import authenticate
 from rest_framework import routers, serializers, viewsets, permissions, status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+import redis
 
-# Two dictionaries for easy reverse lookup.
-logged_in_token = {}
-logged_in_userid = {}
-
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -42,12 +40,13 @@ def login(request, format=None):
     password = request.data['password']
     user = authenticate(username=username, password=password)
     if user is not None:
-        if user.id not in logged_in_userid:
+        token_str = r.get(str(user.id))
+        if token_str is None:
             token = uuid4()
-            logged_in_token[token] = user.id
-            logged_in_userid[user.id] = token
+            r.set(str(user.id), str(token))
+            r.set(str(token), str(user.id))
         else:
-            token = logged_in_userid[user.id]
+            token = UUID(token_str)
         return Response({'token': token, 'userid': user.id})
     return Response("Invalid username/password.", status=status.HTTP_400_BAD_REQUEST)
 
@@ -56,12 +55,11 @@ def login(request, format=None):
 @permission_classes((permissions.AllowAny,))
 def check_token(request, format=None):
     token_str = request.data.get('token', None)
-    if token_str is not None:
-        token = UUID(token_str)
-    else:
+    if token_str is None:
         return Response("Token required.", status=status.HTTP_400_BAD_REQUEST)
-    if token in logged_in_token:
-        return Response({"userid": logged_in_token[token]})
+    userid = r.get(token_str)
+    if userid:
+        return Response({"userid": int(userid)})
     return Response("Invalid token.", status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -69,14 +67,11 @@ def check_token(request, format=None):
 @permission_classes((permissions.AllowAny,))
 def logout(request, format=None):
     token_str = request.data.get('token', None)
-    if token_str is not None:
-        token = UUID(token_str)
-    else:
+    if token_str is None:
         return Response("Token required.", status=status.HTTP_400_BAD_REQUEST)
-    if token in logged_in_token:
-        userid = logged_in_token[token]
-        del logged_in_token[token]
-        del logged_in_userid[userid]
+    if r.get(token_str):
+        userid_str = r.get(token_str)
+        r.delete(token_str, userid_str)
     return Response("User logged out.")
 
 

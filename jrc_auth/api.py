1b41feeb3eb289f5da42828916b0d1f6e86b95e4
Øyvind Robertsen <oyvind@arcticstyle.net>
2017-06-14 14:43:21 +0200

    Start rewrite to use JWT
    JWTs are signed with a device-specific key which is stored in Redis. The key can
    be retrieved by APIs in order to verify the integrity of a JWT.

--- jrc_auth/api.py
+++ jrc_auth/api.py
@@ -1,16 +1,22 @@
-from uuid import uuid4
+import redis
+import jwt
+import hashlib
+
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.shortcuts import render
+from django.http import HttpResponse
 from rest_framework import serializers, viewsets, permissions
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-import redis
+from uuid import uuid4
+from datetime import datetime
 
 from jrc_auth.forms import JrCUserCreationForm
+from jrc_auth.settings import EXPIRATION_TIME, SECRET_SEPARATOR, JWT_SECRET
 
-r = redis.StrictRedis(host='localhost', port=6379, db=0)
-r_act = redis.StrictRedis(host='localhost', port=6379, db=1)
+activation_tokens = redis.StrictRedis(host='localhost', port=6379, db=0)
+user_keys = redis.StrictRedis(host='localhost', port=6379, db=1)
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -37,48 +43,58 @@ class UserViewSet(viewsets.ModelViewSet):
             return User.objects.filter(id=self.request.user.id)
 
 
+def create_session_key(user_id, device_id, issued_at):
+    return \
+        hashlib.md5(
+            str(user_id) +
+            str(device_id) +
+            issued_at.strftime("%s")
+        ).hexdigest()
+
+
+def create_secret(user_key):
+    return ("{}".format(SECRET_SEPARATOR)).join([JWT_SECRET, user_key])
+
+
+def create_jwt(user, device_id, user_key, issued_at, expires_at):
+    payload = {
+        'username': user.username,
+        'userid': user.id,
+        'deviceId': device_id,
+        'jti': create_session_key(user.id, device_id, issued_at),
+        'iat': issued_at,
+        'exp': expires_at
+    }
+    return jwt.encode(payload, create_secret(user_key), algorithm='HS256')
+
+
+def get_jwt(user, device_id):
+    user_key = str(uuid4())
+    issued_at = datetime.utcnow()
+    expires_at = issued_at + EXPIRATION_TIME
+
+    jwt = create_jwt(user, device_id, user_key, issued_at, expires_at)
+    session_key = create_session_key(user.id, device_id, issued_at)
+
+    user_keys.setex(str(session_key), EXPIRATION_TIME, user_key)
+
+    return jwt
+
+
 @api_view(['POST'])
 @permission_classes((permissions.AllowAny,))
 def login(request, format=None):
     username = request.data['username']
     password = request.data['password']
+    device_id = request.data['device_id']
+    if None in (username, password, device_id):
+        return Response({'message':
+                         "username, password, device_id are required fields"})
     user = authenticate(username=username, password=password)
     if user is not None:
-        token_str = r.get(str(user.id))
-        if token_str:
-            r.delete((str(user.id), token_str))
-            r.delete(token_str, str(user.id))
-        token = uuid4()
-        r.set(str(user.id), str(token))
-        r.expire(str(user.id), 86400 * 14)  # Expire in 14 days
-        r.set(str(token), str(user.id))
-        r.expire(str(token), 86400 * 14)
-        return Response({'token': token, 'userid': user.id})
+        token = get_jwt(user, device_id)
+        return Response({'token': token})
     return Response({'message': "Invalid username/password."})
-
-
-@api_view(['POST'])
-@permission_classes((permissions.AllowAny,))
-def check_token(request, format=None):
-    token_str = request.data.get('token', None)
-    if token_str is None:
-        return Response({'message': "Token required."})
-    userid = r.get(token_str)
-    if userid:
-        return Response({"userid": int(userid)})
-    return Response({'message': "Invalid token."})
-
-
-@api_view(['POST'])
-@permission_classes((permissions.AllowAny,))
-def logout(request, format=None):
-    token_str = request.data.get('token', None)
-    if token_str is None:
-        return Response({'message': "Token required."})
-    if r.get(token_str):
-        userid_str = r.get(token_str)
-        r.delete(token_str, userid_str)
-    return Response({'message': "User logged out."})
 
 
 @api_view(['POST'])
@@ -92,12 +108,33 @@ def register(request, format=None):
 
 
 def activate(request, token=None):
-    email = r_act.get(token)
+    email = activation_tokens.get(token)
     if email:
-        r_act.delete(email)
-        r_act.delete(token)
+        activation_tokens.delete(email)
+        activation_tokens.delete(token)
         user = User.objects.get(email=email)
         user.is_active = True
         user.save()
         return render(request, 'jrc_auth/success.html')
     return render(request, 'jrc_auth/error.html')
+
+
+@api_view(['POST'])
+@permission_classes((permissions.AllowAny,))
+def get_user_key(request, session_key=None):
+    if session_key is None:
+        return Response({'message': "session_key is a mandatory parameter"})
+    user_key = user_keys.get(str(session_key))
+    if user_key:
+        return Response({'user_key': user_key})
+    return Response({'message': "Session does not exist or has expired."})
+
+
+@api_view(['POST'])
+@permission_classes((permissions.AllowAny,))
+def delete_user_key(request, session_key=None):
+    if session_key is None:
+        return Response({'message': "session_key is a mandatory parameter"})
+    if user_keys.delete(session_key):
+        return HttpResponse()
+    return Response({'message': "User session expired or already deleted."})
--- jrc_auth/urls.py
+++ jrc_auth/urls.py
@@ -3,13 +3,11 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-from jrc_auth.api import login, check_token, logout, register, activate
+from jrc_auth.api import login, register, activate
 
 
 urlpatterns = [
     url(r'^login/', login),
-    url(r'^check-token', check_token),
-    url(r'^logout/', logout),
     url(r'^register/', register),
     url(r'^activate/(?P<token>.*)/', activate),
     url(r'^admin/', admin.site.urls),
